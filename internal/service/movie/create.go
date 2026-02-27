c0c2b54ab8e78a34078143bd85c1e5979af6664e
Shahrzad Taherzadeh <shahrzad.taherzadeh12@gmail.com>
2026-02-28 00:06:33 +0330

    fix: update Movie create service logic and fix concurrency issues

--- internal/service/movie/create.go
+++ internal/service/movie/create.go
@@ -1,38 +1,13 @@
 package movie
 
 import (
-	"errors"
+	"time"
+
 	"github.com/Shahrzad-Taherzadeh/cinemaTicket/internal/model"
 )
 
-type Repository interface {
-	Create(m *model.Movie) error
-}
-
-type Service struct {
-	repo Repository
-}
-
-func NewService(r Repository) *Service {
-	return &Service{repo: r}
-}
-
-func (s *Service) Create(m *model.Movie) error {
-	if m.Name == "" {
-		return errors.New("movie name cannot be empty")
-	}
-	if m.Year <= 1800 || m.Year > 2100 {
-		return errors.New("movie year is invalid")
-	}
-	if m.Genre == "" {
-		return errors.New("movie genre cannot be empty")
-	}
-	if m.Director == "" {
-		return errors.New("movie director cannot be empty")
-	}
-	if m.DurationMinutes <= 0 {
-		return errors.New("movie duration must be greater than zero")
-	}
-
-	return s.repo.Create(m)
+func (s *Service) Create(input model.Movie) (model.Movie, error) {
+	input.CreatedAt = time.Now()
+	input.UpdatedAt = time.Now()
+	return s.repo.Create(input)
 }
